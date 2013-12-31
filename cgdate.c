d2cdb339110b53708503d201fae4c5098e67ccbf
Andrew Plotkin <erkyrath@eblong.com>
2013-12-30 20:39:59 -0500

    Fixed a struct initialization bug in gli_date_to_tm(). (I think this     caused no problems in practice.)

--- cgdate.c
+++ cgdate.c
@@ -42,7 +42,7 @@ static glsi32 gli_date_to_tm(glkdate_t *date, struct tm *tm)
 {
     glsi32 microsec;
 
-    bzero(tm, sizeof(tm));
+    bzero(tm, sizeof(*tm));
     tm->tm_year = date->year - 1900;
     tm->tm_mon = date->month - 1;
     tm->tm_mday = date->day;
