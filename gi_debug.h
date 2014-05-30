a849619e6039f65a43939b2a729c249312ee7738
Andrew Plotkin <erkyrath@eblong.com>
2014-05-30 18:41:23 -0400

    Start a big long comment.

--- gi_debug.h
+++ gi_debug.h
@@ -1,6 +1,57 @@
 #ifndef _GI_DEBUG_H
 #define _GI_DEBUG_H
 
+/* gi_debug.h: Debug feature layer for Glk API.
+    gi_debug version 0.9.0
+    Designed by Andrew Plotkin <erkyrath@eblong.com>
+    http://eblong.com/zarf/glk/
+
+    This file is copyright 2014 by Andrew Plotkin. You may copy,
+    distribute, and incorporate it into your own programs, by any means
+    and under any conditions, as long as you do not modify it. You may
+    also modify this file, incorporate it into your own programs,
+    and distribute the modified version, as long as you retain a notice
+    in your program or documentation which mentions my name and the URL
+    shown above.
+
+    ------------------------------------------------
+
+    The debug module allows a Glk library to send out-of-band debug
+    commands to the game program it's linked to. (Most familiarly this
+    is a Glulx interpreter, but it doesn't have to be.) The program
+    returns debug output to the library, which can then display it.
+
+    As with all UI decision, the interface of the debug feature is
+    left up to the Glk library. Abstractly, we imagine a "debug
+    console" window with its own input line and scrolling text output.
+
+    The debug feature is cooperative: both the library and the game
+    must support it for debug commands to work. This requires a dance
+    of #ifdefs to make sure that everything builds in all
+    configurations.
+
+    Library side: If the library supports debugging, the
+    GIDEBUG_LIBRARY_SUPPORT #define in this header (gi_debug.h) will
+    be present (not commented out). By doing this, the library
+    declares that it offers the functions gidebug_output() and
+    gidebug_pause().
+
+    Older Glk libraries do not include this header at all. Therefore,
+    a game (interpreter) should have its own configuration option.
+    For example, in Glulxe, you define VM_DEBUGGER when compiling with
+    a library that has this header and GIDEBUG_LIBRARY_SUPPORT defined.
+    When building with an older library (or a library which comments
+    out the GIDEBUG_LIBRARY_SUPPORT line), you don't define VM_DEBUGGER,
+    and then the interpreter does not attempt to call debug APIs.
+
+    Game (interpreter) side: If the interpreter supports debug commands,
+    it should call gidebug_debugging_available() in its startup code.
+    (See unixstrt.c in the Glulxe source.) If it does not do this, the
+    library knows that debug commands cannot be handled; it should
+    disable or hide the "debug console" UI.
+*/
+
+
 /* ###debug Uncomment if the library supports a UI for debug commands. */
 #define GIDEBUG_LIBRARY_SUPPORT (1)
 
