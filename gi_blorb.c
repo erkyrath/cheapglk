d652a2e842b6e75b4e4b49c39ede9dfcdc2f6434
Andrew Plotkin <zarf@Zarfslab.local>
2017-02-13 18:07:23 -0500

    Update gi_blorb to support image data. The CheapGlk library can't do anything with images, but other libraries can, and we want to use the same gi_blorb files.

--- gi_blorb.c
+++ gi_blorb.c
@@ -1,9 +1,9 @@
 /* gi_blorb.c: Blorb library layer for Glk API.
-    gi_blorb version 1.5.1.
+    gi_blorb version 1.6.0.
     Designed by Andrew Plotkin <erkyrath@eblong.com>
     http://eblong.com/zarf/glk/
 
-    This file is copyright 1998-2012 by Andrew Plotkin. It is
+    This file is copyright 1998-2017 by Andrew Plotkin. It is
     distributed under the MIT license; see the "LICENSE" file.
 */
 
@@ -43,7 +43,8 @@ typedef struct giblorb_chunkdesc_struct {
     
     void *ptr; /* pointer to malloc'd data, if loaded */
     int auxdatnum; /* entry in the auxsound/auxpict array; -1 if none.
-        This only applies to chunks that represent resources;  */
+        This only applies to chunks that represent resources. 
+        (Currently, only images.) */
     
 } giblorb_chunkdesc_t;
 
@@ -53,6 +54,14 @@ typedef struct giblorb_resdesc_struct {
     glui32 resnum;
     glui32 chunknum;
 } giblorb_resdesc_t;
+
+/* giblorb_auxpict_t: Extra information about an image. */
+typedef struct giblorb_auxpict_struct {
+    int loaded;
+    glui32 width;
+    glui32 height;
+    char *alttext;
+} giblorb_auxpict_t;
 
 /* giblorb_map_t: Holds the complete description of an open Blorb file. */
 struct giblorb_map_struct {
@@ -67,6 +76,8 @@ struct giblorb_map_struct {
     giblorb_resdesc_t *resources; /* list of resource descriptors */
     giblorb_resdesc_t **ressorted; /* list of pointers to descriptors 
         in map->resources -- sorted by usage and resource number. */
+
+    giblorb_auxpict_t *auxpict;
 };
 
 #define giblorb_Inited_Magic (0xB7012BED) 
@@ -77,6 +88,8 @@ static int lib_inited = FALSE;
 
 static giblorb_err_t giblorb_initialize(void);
 static giblorb_err_t giblorb_initialize_map(giblorb_map_t *map);
+static giblorb_err_t giblorb_image_get_size_jpeg(unsigned char *ptr, glui32 length, giblorb_auxpict_t *auxpict);
+static giblorb_err_t giblorb_image_get_size_png(unsigned char *ptr, glui32 length, giblorb_auxpict_t *auxpict);
 static void giblorb_qsort(giblorb_resdesc_t **list, int len);
 static giblorb_resdesc_t *giblorb_bsearch(giblorb_resdesc_t *sample, 
     giblorb_resdesc_t **list, int len);
@@ -199,8 +212,8 @@ giblorb_err_t giblorb_create_map(strid_t file, giblorb_map_t **newmap)
     map->resolution = NULL;
     map->palettechunk = -1;
     map->palette = NULL;
-    map->auxsound = NULL;
-    map->auxpict = NULL;*/
+    map->auxsound = NULL;*/
+    map->auxpict = NULL;
     
     /* Now we do everything else involved in loading the Blorb file,
         such as building resource lists. */
@@ -227,7 +240,8 @@ static giblorb_err_t giblorb_initialize_map(giblorb_map_t *map)
     char *ptr;
     glui32 len;
     glui32 numres;
-    int gotindex = FALSE; 
+    int gotindex = FALSE;
+    int pictcount = 0;
 
     for (ix=0; ix<map->numchunks; ix++) {
         giblorb_chunkdesc_t *chu = &map->chunks[ix];
@@ -308,7 +322,27 @@ static giblorb_err_t giblorb_initialize_map(giblorb_map_t *map)
                 giblorb_unload_chunk(map, ix);
                 gotindex = TRUE;
                 break;
+
+            case giblorb_ID_JPEG:
+            case giblorb_ID_PNG:
+                chu->auxdatnum = pictcount;
+                pictcount++;
+                break;
             
+        }
+    }
+
+    if (pictcount) {
+        map->auxpict = (giblorb_auxpict_t *)giblorb_malloc(pictcount 
+            * sizeof(giblorb_auxpict_t));
+        if (!map->auxpict)
+            return giblorb_err_Alloc;
+        for (ix=0; ix<pictcount; ix++) {
+            giblorb_auxpict_t *auxpict = &(map->auxpict[ix]);
+            auxpict->loaded = FALSE;
+            auxpict->width = 0;
+            auxpict->height = 0;
+            auxpict->alttext = NULL;
         }
     }
     
@@ -321,6 +355,11 @@ giblorb_err_t giblorb_destroy_map(giblorb_map_t *map)
     
     if (!map || !map->chunks || map->inited != giblorb_Inited_Magic)
         return giblorb_err_NotAMap;
+
+    if (map->auxpict) {
+        giblorb_free(map->auxpict);
+        map->auxpict = NULL;
+    }
     
     for (ix=0; ix<map->numchunks; ix++) {
         giblorb_chunkdesc_t *chu = &(map->chunks[ix]);
@@ -501,6 +540,120 @@ giblorb_err_t giblorb_count_resources(giblorb_map_t *map, glui32 usage,
         *max = maxval;
     
     return giblorb_err_None;
+}
+
+giblorb_err_t giblorb_load_image_info(giblorb_map_t *map,
+    glui32 resnum, giblorb_image_info_t *res)
+{
+    giblorb_resdesc_t sample;
+    giblorb_resdesc_t *found;
+    
+    sample.usage = giblorb_ID_Pict;
+    sample.resnum = resnum;
+    
+    found = giblorb_bsearch(&sample, map->ressorted, map->numresources);
+    
+    if (!found)
+        return giblorb_err_NotFound;
+    
+    glui32 chunknum = found->chunknum;
+    if (chunknum >= map->numchunks)
+        return giblorb_err_NotFound;
+
+    giblorb_chunkdesc_t *chu = &(map->chunks[chunknum]);
+    if (chu->auxdatnum < 0)
+        return giblorb_err_NotFound;
+
+    giblorb_auxpict_t *auxpict = &(map->auxpict[chu->auxdatnum]);
+    if (!auxpict->loaded) {
+        giblorb_result_t res;
+        giblorb_err_t err = giblorb_load_chunk_by_number(map, giblorb_method_Memory, &res, chunknum);
+        if (err)
+            return err;
+
+        if (chu->type == giblorb_ID_JPEG)
+            err = giblorb_image_get_size_jpeg(res.data.ptr, res.length, auxpict);
+        else if (chu->type == giblorb_ID_PNG)
+            err = giblorb_image_get_size_png(res.data.ptr, res.length, auxpict);
+        else
+            err = giblorb_err_Format;
+
+        giblorb_unload_chunk(map, chunknum);
+
+        if (err)
+            return err;
+
+        auxpict->loaded = TRUE;
+    }
+
+    res->chunktype = chu->type;
+    res->width = auxpict->width;
+    res->height = auxpict->height;
+    res->alttext = auxpict->alttext;
+    return giblorb_err_None;
+}
+
+static giblorb_err_t giblorb_image_get_size_jpeg(unsigned char *arr, glui32 length, giblorb_auxpict_t *auxpict)
+{
+    int pos = 0;
+    while (pos < length) {
+        if (arr[pos] != 0xFF) {
+            /* error: find_dimensions_jpeg: marker is not 0xFF */
+            return giblorb_err_Format;
+        }
+        while (arr[pos] == 0xFF) 
+            pos += 1;
+        unsigned char marker = arr[pos];
+        pos += 1;
+        if (marker == 0x01 || (marker >= 0xD0 && marker <= 0xD9)) {
+            /* marker type has no data */
+            continue;
+        }
+        int chunklen = (arr[pos+0] << 8) | (arr[pos+1]);
+        if (marker >= 0xC0 && marker <= 0xCF && marker != 0xC8) {
+            if (chunklen < 7) {
+                /* error: find_dimensions_jpeg: SOF block is too small */
+                return giblorb_err_Format;
+            }
+            auxpict->height = (arr[pos+3] << 8) | (arr[pos+4]);
+            auxpict->width  = (arr[pos+5] << 8) | (arr[pos+6]);
+            return giblorb_err_None;
+        }
+        pos += chunklen;
+    }
+
+    /* error: find_dimensions_jpeg: no SOF marker found */
+    return giblorb_err_Format;
+}
+
+static giblorb_err_t giblorb_image_get_size_png(unsigned char *arr, glui32 length, giblorb_auxpict_t *auxpict)
+{
+    int pos = 0;
+    if (length < 8)
+        return giblorb_err_Format;
+    if (arr[0] != 0x89 || arr[1] != 'P' || arr[2] != 'N' || arr[3] != 'G') {
+        /* error: find_dimensions_png: PNG signature does not match */
+        return giblorb_err_Format;
+    }
+    pos += 8;
+    while (pos < length) {
+        glui32 chunklen = giblorb_native4(arr+pos);
+        pos += 4;
+        glui32 chunktype = giblorb_native4(arr+pos);
+        pos += 4;
+        if (chunktype == giblorb_make_id('I', 'H', 'D', 'R')) {
+            auxpict->width = giblorb_native4(arr+pos);
+            pos += 4;
+            auxpict->height = giblorb_native4(arr+pos);
+            pos += 4;
+            return giblorb_err_None;
+        }
+        pos += chunklen;
+        pos += 4; /* skip CRC */
+    }
+
+    /* error: find_dimensions_png: no PNG header block found */
+    return giblorb_err_Format;
 }
 
 /* Sorting and searching. */
